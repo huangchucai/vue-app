4b6140bf9761d09945239814082560d98ad96f64
sunny <sunny@lianj.com>
2017-12-01 20:52:30 +0800

    silider处理

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -4,7 +4,7 @@
 import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
 
-export default function getRecommend() {
+export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
   const data = Object.assign({}, commonParams, {
     platform: 'h5',
---
+++ src/base/slider/slider.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot>
+      </slot>
+    </div>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+  export default {
+    props: {
+      loop: {
+        default: true,
+        type: Boolean
+      },
+      autoPlay: {
+        default: true,
+        type: Boolean
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._setSlideWidth()  // 设置滑动的父元素宽度
+        this._initSlider() // 初始化滑动
+      }, 20)
+    },
+    methods: {
+      _setSlideWidth() {
+        this.children = this.$refs.sliderGroup.children
+        let parentWidth = 0
+        let imgWrapperWidth = this.$refs.slider.clientWidth
+        Array.prototype.forEach.call(this.children, imgDom => {
+          imgDom.classList.add('slider-item')
+          imgDom.style.width = imgWrapperWidth + 'px'
+          parentWidth += imgWrapperWidth
+        })
+        if (this.loop) {
+          parentWidth += 2 * imgWrapperWidth
+        }
+        this.$refs.sliderGroup.style.width = parentWidth + 'px'
+      },
+      _initSlider() {
+        this.slider = new BScroll(this.$refs.slider, {
+          scrollX: true,
+          scrollY: false,
+          momentum: false,
+          snap: true,
+          snapLoop: this.loop,
+          snapThreshold: 0.3,
+          snapSpeed: 400
+        })
+      }
+    }
+  }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  .slider
+    min-height: 1px
+    .slider-group
+      position: relative
+      overflow: hidden
+      white-space: nowrap
+      .slider-item
+        float: left
+        box-sizing: border-box
+        overflow: hidden
+        text-align: center
+        a
+          display: block
+          width: 100%
+          overflow: hidden
+          text-decoration: none
+        img
+          display: block
+          width: 100%
+    .dots
+      position: absolute
+      right: 0
+      left: 0
+      bottom: 12px
+      text-align: center
+      font-size: 0
+      .dot
+        display: inline-block
+        margin: 0 4px
+        width: 8px
+        height: 8px
+        border-radius: 50%
+        background: $color-text-l
+        &.active
+          width: 20px
+          border-radius: 5px
+          background: $color-text-ll
+</style>
